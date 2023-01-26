ab8469e0605240f50bcb78d1276e791fa5532a48
iftach <iftach.sella@gmail.com>
2023-01-26 20:09:10 +0000

    Implemented lexer and refactored parser

--- src/main/scala/wacc/Parser.scala
+++ src/main/scala/wacc/Parser.scala
@@ -4,34 +4,70 @@ import wacc.AbstractSyntaxTree.UnaryOpType._
 import wacc.AbstractSyntaxTree.BinaryOpType._
 import parsley.Parsley
 import parsley.combinator.{choice, some}
-import parsley.lift.lift2
 import parsley.Parsley.{attempt, notFollowedBy, pure}
-import parsley.character.{char, digit, letter, letterOrDigit, satisfy, string, stringOfMany, stringOfSome}
+import parsley.character.{letterOrDigit, stringOfMany}
 import parsley.expr.{InfixL, Ops, Prefix, precedence}
-import parsley.implicits.character.{charLift, stringLift}
+import parsley.implicits.character.charLift
+
+object Lexer {
+
+  import parsley.token.descriptions.numeric.NumericDesc
+  import parsley.token.descriptions.text.{EscapeDesc, TextDesc}
+  import parsley.token.descriptions.{LexicalDesc, NameDesc, SpaceDesc, SymbolDesc}
+  import parsley.token.predicate
+  import parsley.token.Lexer
+
+  private val desc = LexicalDesc.plain.copy(
+    nameDesc = NameDesc.plain.copy(
+      identifierStart = predicate.Basic((c: Char) => (c == '_') || c.isLetter),
+      identifierLetter = predicate.Basic((c: Char) => (c == '_') || c.isLetterOrDigit)
+    ),
+    symbolDesc = SymbolDesc.plain.copy(
+      hardKeywords = Set("true", "false", "null", "len", "ord", "chr"),
+      hardOperators = Set("!", "*", "/", "%", "+", "-", ">=", ">", "<", "<=", "==", "!=", "&&", "||")
+    ),
+    numericDesc = NumericDesc.plain.copy(
+    ),
+    textDesc = TextDesc.plain.copy(
+      escapeSequences = EscapeDesc.plain.copy() //TODO: Add escape characters
+    ),
+    spaceDesc = SpaceDesc.plain.copy(
+      commentLine = "#",
+      space = predicate.Basic(Character.isWhitespace)
+    )
+  )
+
+  private val lexer = new Lexer(desc)
+  val integer: Parsley[Int] = lexer.lexeme.numeric.signed.decimal32
+  val character: Parsley[Char] = lexer.lexeme.text.character.ascii
+  val boolean: Parsley[Boolean] = (lexer.lexeme.symbol.apply("true", "true") #> true) <|>
+                                  (lexer.lexeme.symbol.apply("false", "false") #> false)
+  val string: Parsley[String] = lexer.lexeme.text.string.ascii
+  val emptyPair: Parsley[Unit] = lexer.lexeme.symbol.apply("null", "null")
+  val identifier: Parsley[String] = lexer.lexeme.names.identifier
+
+  def fully[A](p: Parsley[A]): Parsley[A] = lexer.fully(p)
+
+  val implicits = lexer.lexeme.symbol.implicits
+}
 
 object Parser {
-  val integer: Parsley[Int] = stringOfSome(digit).map(_.toInt)
-  val bool: Parsley[Boolean] = ("true" #> true) <|> ("false" #> false)
-  //  var lexer = new LexicalDesc()
-  object Expression {
-    //TODO:
-    private lazy val escapedChar = (char('\\') ~> letter).map(identity[Char])
-    private def withBracket[A](p: Parsley[A], bracket: Char): Parsley[A] = bracket ~> p <~ bracket
-    private lazy val sign: Parsley[Int => Int] = '+' #> (identity[Int] _) <|> '-' #> (x => -x)
+  import Lexer._
 
-    private lazy val intLiteral = (sign <*> integer <|> integer).map(IntLiteral)
-    private lazy val boolLiteral = bool.map(BoolLiteral)
-    private lazy val charLiteral = withBracket((letter <|> escapedChar), '\'').map(CharLiteral)
-    private lazy val stringLiteral = withBracket(stringOfMany(satisfy(_ != '"') <|> escapedChar), '"').map(StringLiteral)
-    private lazy val pairLiteral = attempt((string("null") ~> notFollowedBy(letterOrDigit)) #> PairLiteral())
-    private lazy val identStart = ('_' <|> letter)
-    private lazy val identCont = stringOfMany('_' <|> letterOrDigit)
-    private lazy val ident = lift2((a: Char, b: String) => (a + b), identStart, identCont)
+  object Expression {
+    import Lexer.implicits._
+
+    private lazy val intLiteral = integer.map(IntLiteral)
+    private lazy val boolLiteral = boolean.map(BoolLiteral)
+    private lazy val charLiteral = character.map(CharLiteral)
+    private lazy val stringLiteral = Lexer.string.map(StringLiteral)
+    private lazy val pairLiteral = emptyPair #> PairLiteral()
+    private lazy val ident = identifier
     private lazy val maybeArrayElem: Parsley[String => Expr] = {
-      choice(some('[' ~> parseExpr <~ ']').map(ArrayElem(_)), pure(IdentLiteral(_)))
+      choice(some('[' ~> _parseExpr <~ ']').map(ArrayElem(_)), pure(IdentLiteral(_)))
     }
-    private lazy val parseExprAtom: Parsley[Expr] =
+
+    lazy val parseExprAtom: Parsley[Expr] =
       intLiteral <|>
         boolLiteral <|>
         charLiteral <|>
@@ -40,14 +76,13 @@ object Parser {
         (ident <**> maybeArrayElem) // Both an identifier and an array element can start with an 'ident'
 
     // Not the nicest solution, but it works for now...:
-    private def without[A](q: Parsley[A], p: Parsley[A]) = attempt (p <~ notFollowedBy(q))
+    private def without[A](q: Parsley[A], p: Parsley[A]): Parsley[A] = attempt(p <~ notFollowedBy(q))
+    private lazy val identCont = stringOfMany('_' <|> letterOrDigit)
 
-    lazy val parseExpr: Parsley[Expr] = precedence(
-      Ops[Expr](Prefix)('!' #> UnaryOp(Not),
-        without(intLiteral, '-') #> UnaryOp(Neg),
-        without(identCont, "len") #> UnaryOp(Len),
-        without(identCont, "ord") #> UnaryOp(Ord),
-        without(identCont, "chr") #> UnaryOp(Chr)),
+    private lazy val _parseExpr: Parsley[Expr] = precedence(parseExprAtom, '(' ~> _parseExpr <~ ')')(
+      Ops[Expr](Prefix)(without(intLiteral, "-") #> UnaryOp(Neg),
+        "!" #> UnaryOp(Not), "len" #> UnaryOp(Len),
+        "ord" #> UnaryOp(Ord), "chr" #> UnaryOp(Chr)),
       Ops[Expr](InfixL)("*" #> BinaryOp(Mul), "/" #> BinaryOp(Div), "%" #> BinaryOp(Mod)),
       Ops[Expr](InfixL)("+" #> BinaryOp(Add), "-" #> BinaryOp(Sub)),
       Ops[Expr](InfixL)(">=" #> BinaryOp(Gte), ">" #> BinaryOp(Gt)),
@@ -55,6 +90,8 @@ object Parser {
       Ops[Expr](InfixL)("==" #> BinaryOp(Eq), "!=" #> BinaryOp(Neq)),
       Ops[Expr](InfixL)("&&" #> BinaryOp(And)),
       Ops[Expr](InfixL)("||" #> BinaryOp(Or))
-    )(parseExprAtom <|> ('(' ~> parseExpr <~ ')'))
+    )
+
+    lazy val parseExpr: Parsley[Expr] = fully(_parseExpr)
   }
 }
--- src/test/scala/wacc/ParserSpec.scala
+++ src/test/scala/wacc/ParserSpec.scala
@@ -1,31 +1,32 @@
 package wacc
 import org.scalatest.flatspec.AnyFlatSpec
-import parsley.{Failure, Success}
-import Parser.Expression.parseExpr
+import parsley.Success
+import Parser.Expression.{parseExpr}
 import wacc.AbstractSyntaxTree._
+import wacc.AbstractSyntaxTree.UnaryOpType._
+import wacc.AbstractSyntaxTree.BinaryOpType._
 class ParserSpec extends AnyFlatSpec {
 
-  "Expression Parser" can "evaluate positive integers" in {
-    assert(parseExpr.parse("+123") == Success(IntLiteral(123)))
+  "Expression Parser" can "parse positive integers" in {
+    assert(parseExpr.parse("123") == Success(IntLiteral(123)))
   }
-
-  "Expression Parser" can "evaluate negative integers" in {
+  "Expression Parser" can "parse negative integers" in {
     assert(parseExpr.parse("-123") == Success(IntLiteral(-123)))
   }
 
-  "Expression Parser" can "evaluate 'true'" in {
+  "Expression Parser" can "parse 'true'" in {
     assert(parseExpr.parse("true") == Success(BoolLiteral(true)))
   }
 
-  "Expression Parser" can "evaluate 'false'" in {
+  "Expression Parser" can "parse 'false'" in {
     assert(parseExpr.parse("false") == Success(BoolLiteral(false)))
   }
 
-  "Expression Parser" can "evaluate a character" in {
+  "Expression Parser" can "parse a character" in {
     assert(parseExpr.parse("\'a\'") == Success(CharLiteral('a')))
   }
 
-  "Expression Parser" can "evaluate a string" in {
+  "Expression Parser" can "parse a string" in {
     assert(parseExpr.parse("\"+ - ' true false\"") == Success(StringLiteral("+ - ' true false")))
   }
 
@@ -43,7 +44,12 @@ class ParserSpec extends AnyFlatSpec {
   }
 
   "Expression Parser" should "not confuse literals and unary operations" in {
-    assert(parseExpr.parse("len_var") == Success(IdentLiteral("len_var")))
+    assert(parseExpr.parse("lenvar") == Success(IdentLiteral("lenvar")))
+  }
+
+  "Expression Parser" should "use correct binding when parsing prefix ops" in {
+    assert(parseExpr.parse("!b + 1") ==
+      Success(BinaryOp(BinaryOpType.Add)((UnaryOp(Not)(IdentLiteral("b"))), IntLiteral(1))))
   }
 
   /*TODO: This test tests for behaviour which is allowed by the syntax of the language (as defined in the spec) -
@@ -51,8 +57,9 @@ class ParserSpec extends AnyFlatSpec {
       This should raise an exception during semantic analysis, but it might be pretty easy to modify the parser so that these errors
        already get detected at this stage.*/
   "Expression Parser" can "parse array identifiers" in {
-    assert(parseExpr.parse("_literal_123[123][true][_literal_321_[321]]") == Success(ArrayElem(List(IntLiteral(123),
-      BoolLiteral(true), ArrayElem(List(IntLiteral(321)))("_literal_321_")))("_literal_123")))
+    assert(parseExpr.parse("_literal_123[123][true][_literal_321_[321]]") ==
+      Success(ArrayElem(List(IntLiteral(123),
+        BoolLiteral(true), ArrayElem(List(IntLiteral(321)))("_literal_321_")))("_literal_123")))
   }
 
   "Expression Parser" can "fail when array indices don't match" in {
@@ -60,11 +67,21 @@ class ParserSpec extends AnyFlatSpec {
   }
 
   "Expression Parser" should "parse binary operations greedily" in {
-    assert(parseExpr.parse ("a<=b") == Success(BinaryOp(BinaryOpType.Lte, IdentLiteral("a"), IdentLiteral("b"))))
+    assert(parseExpr.parse("a<=b") == Success(BinaryOp(Lte, IdentLiteral("a"), IdentLiteral("b"))))
+  }
+
+  "Expression Parser" can "parse unary operations" in {
+    val ops = Set("chr" -> Chr, "len" -> Len, "ord" -> Ord, "-" -> Neg, "!" -> Not)
+    for (op <- ops) {
+      assert(parseExpr.parse("a + " + op._1 + " b") == Success(BinaryOp(Add)(IdentLiteral("a"), UnaryOp(op._2)(IdentLiteral("b")))))
+    }
   }
 
   "Expression Parser" should "parse bracketed expressions correctly" in {
-    assert(parseExpr.parse("4*(a<=b)") == Success(BinaryOp(BinaryOpType.Mul)(IntLiteral(4), BinaryOp(BinaryOpType.Lte)(IdentLiteral("a"), IdentLiteral("b")))))
+    assert(parseExpr.parse("4*(a<=b)") == Success(BinaryOp(Mul)(IntLiteral(4), BinaryOp(BinaryOpType.Lte)(IdentLiteral("a"), IdentLiteral("b")))))
   }
 
+  "Expression Parser" should "respect precedence of operations" in {
+    assert(parseExpr.parse("1 * 2 + len x") == Success(BinaryOp(Add)(BinaryOp(Mul)(IntLiteral(1), IntLiteral(2)), UnaryOp(Len)(IdentLiteral("x")))))
+  }
 }
