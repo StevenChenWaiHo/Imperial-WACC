6fe373df1ece05fa5e72d99e52fa03025139030f
iftach <iftach.sella@gmail.com>
2023-01-27 00:08:02 +0000

    Added AST entries for statements

--- src/main/scala/wacc/AbstractSyntaxTree.scala
+++ src/main/scala/wacc/AbstractSyntaxTree.scala
@@ -1,27 +1,29 @@
 package wacc
 
 import wacc.AbstractSyntaxTree.BaseT._
-import wacc.AbstractSyntaxTree.BinaryOpType.BinOp
+import wacc.AbstractSyntaxTree.BinaryOpType.{Add, BinOp}
 import wacc.AbstractSyntaxTree.UnaryOpType.UnOp
 
 // This should probably be a class which takes in a lookup table
 object AbstractSyntaxTree {
-  object BaseT extends Enumeration {
-    type Type = Value
-    val Int_T, Bool_T, Char_T, String_T, None_T = Value
-  }
 
-  sealed trait Expr
+  sealed trait PairLit extends Expr with RValue
+  case class PairLiteral() extends PairLit
+
+  sealed trait ArrayE extends Expr with LValue
+  case class ArrayElem(val name: String, val indices: List[Expr]) extends ArrayE
+
+  sealed trait IdentLit extends Expr with LValue
+  case class IdentLiteral(val name: String) extends IdentLit
+
+
+  sealed trait Expr extends RValue
   case class IntLiteral(val x: Int) extends Expr
   case class BoolLiteral(val x: Boolean) extends Expr
   case class CharLiteral(val x: Char) extends Expr
   case class StringLiteral(val x: String) extends Expr
-  case class PairLiteral() extends Expr
-  case class IdentLiteral(val name: String) extends Expr
-  case class ArrayElem(val name: String, val indices: List[Expr]) extends Expr
   case class UnaryOp(val op: UnOp, val expr: Expr) extends Expr
   case class BinaryOp(val op: BinOp, val expr1: Expr, val expr2: Expr) extends Expr
-
 
   /* Constructors and Factories */
   object ArrayElem {
@@ -47,7 +49,36 @@ object AbstractSyntaxTree {
     val Mul, Div, Mod, Add, Sub, Gt, Gte, Lt, Lte, Eq, Neq, And, Or = Value
   }
 
-  case class baseType()
+  sealed trait Stat
+  case class Declaration(dataType: BaseType, ident: String, rvalue: RValue) extends Stat
+  case class Assignment(lvalue: LValue, rvalue: RValue)
+  case class Command(command: CmdT.Cmd, expr: Expr)
+  case class IfStat(cond: Expr, stat1: Stat, stat2: Stat)
+  case class WhileLoop(cond: Expr, stat: Stat)
+  case class Program(stat: Stat)
+  case class StatList(stat1: Stat, stat2: Stat)
+
+  object CmdT extends Enumeration {
+    type Cmd = Value
+    val Free, Return, Exit, Print, PrintLn = Value
+  }
+  object BaseT extends Enumeration {
+    type BaseType = Value
+    val Int_T, Bool_T, Char_T, String_T = Value
+  }
+
+  sealed trait PairElem extends LValue with RValue
+  case class PairValue(exp1: Expr, exp2: Expr) extends PairElem
+
+
+  sealed trait RValue
+  case class ArrayLiteral(elements: List[Expr]) extends RValue
+  case class Call(ident: String, args: List[Expr]) extends RValue
+
+
+  sealed trait LValue
+  case class IdentReference(name: String)
+
 
 }
 
