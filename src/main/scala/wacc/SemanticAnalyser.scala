862cd1d665523ab31102044191328ae7ea73a748
Jing Ngan <jn520@doc.ic.ac.uk>
2023-02-03 13:11:11 +0000

    Removed a file

--- src/main/scala/wacc/SemanticAnalyser.scala
+++ src/main/scala/wacc/SemanticAnalyser.scala
@@ -7,10 +7,16 @@ object SemanticAnalyser {
   private def validateType(n:ASTNode, context: Map[IdentLiteral, FunctionType]): Boolean = {
 
   }
-  private def verifyStat(stat: AbstractSyntaxTree.Stat, expectedFunctionType: FunctionType): List[Error] = {
+  private def verifyStat(stat: AbstractSyntaxTree.Stat, expectedFunctionType: FunctionType): List[Errors] = {
+    val errorsList= List[Errors]
     stat match {
       case skipStat => {/*Pass by default*/}
-      case Declaration(dataType, ident, rvalue) => {/*if ident is keyword then return an error and make sure dataType and rvalue have same type*/}
+      case Declaration(dataType, ident, rvalue) => {
+        /*if ident is keyword then return an error*/
+        dataType match{
+          case NestedPair() => {}
+          case BaseType(baseType) => {}
+        }/*and make sure dataType and rvalue have same type*/}
       case Assignment(lvalue, rvalue) => {/*if lvalue type == rvalue type then true otherwise error*/}
       case Read(lvalue) => {/*Not sure what this is*/}
       case Command(command, input) => {/*Not sure what this is*/}
