f76c054c233be948a39cec87c35de8a98cd5bca1
tyw21 <tyw21@ic.ac.uk>
2023-02-08 16:43:15 +0000

    Added basic TypeValidator tests

---
+++ src/test/scala/wacc/TypeValidatorSpec.scala
@@ -0,0 +1,39 @@
+package wacc
+
+import org.scalatest.flatspec.AnyFlatSpec
+import parsley.Success
+import wacc.AbstractSyntaxTree._
+import wacc.Parser.StatementParser.statement
+import wacc.TypeValidator._
+
+
+class TypeValidatorSpec extends AnyFlatSpec {
+import wacc.ValidateExamples._
+
+  "Type Validator" can "validate base-type literals" in {
+    for (base <- BaseLiteralExamples) {
+      assert(returnType(base._1) == base._2)
+    }
+  }
+
+  // returnType is a placeholder function below
+
+  "Type Validator" can "validate ident-type literals" in {
+    for (ident <- IdentLiteralExamples) {
+      var result = statement.parse(ident._1) // should be IdentLiteral("spam")
+      assert(returnType(result) == ident._2)
+    }
+  }
+
+  "Type Validator" can "validate array-type literals" in {
+    for (array <- ArrayTypeExamples) {
+      assert(returnType(array._1) == array._2)
+    }
+  }
+
+  "Type Validator" can "validate pair-type literals" in {
+    for (pair <- PairTypeExamples) {
+      assert(returnType(pair._1) == pair._2)
+    }
+  }
+}
