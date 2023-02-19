125e8323bd9542b6ce076a2505837d73bc32c96d
Jing Ngan <jn520@doc.ic.ac.uk>
2023-02-19 15:49:02 +0000

    added a translate program

--- src/main/scala/wacc/Translator.scala
+++ src/main/scala/wacc/Translator.scala
@@ -1,5 +1,15 @@
 package wacc
 
+import AbstractSyntaxTree.{Func, Stat}
+
 object Translator {
   //TODO: Translate each ASTNode into ARM
+  def translateProgram(l: List[Func], s: Stat, context: ScopeContext): List[String] = {
+    val str = List("")
+    for (function: Func <- l) {
+      str = str ++ delegateASTNode(function, context)//Not actually sure about the structure of this thing
+    }
+    str = str ++ delegateASTNode(s, context)
+    return str
+  }
 }
