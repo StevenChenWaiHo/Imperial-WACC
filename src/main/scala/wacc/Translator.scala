18b01b40a5de46feacdb92301185c75e3cebea55
rob <rgw20@ic.ac.uk>
2023-02-19 15:50:09 +0000

    Implement ASTNode translaion delegator

--- src/main/scala/wacc/Translator.scala
+++ src/main/scala/wacc/Translator.scala
@@ -1,5 +1,41 @@
 package wacc
 
+import wacc.AbstractSyntaxTree.{ASTNode, Stat, SkipStat, BeginEndStat, Command, Program, Func}
+
 object Translator {
   //TODO: Translate each ASTNode into ARM
+  def delegateASTNode(node: ASTNode, context : ScopeContext) : List[String] = {
+    node match {
+      case Program(funcs, stat) => translateProgram(funcs, stat)
+      case BeginEndStat(stat) => translateBeginEnd(stat)
+      case SkipStat() => translateSkip()
+      case Command(cmd, expr) => translateCommand(cmd, expr)
+      case Func(returnType, ident, types, code) => translateFunction(returnType, ident, types, code)
+      case _ => List("")
+    }
+  }
+
+  def translateProgram(funcs : List[ASTNode], stat : Stat) : List[String] = {
+    List("")
+  }
+
+  def translateBeginEnd(stat : Stat) : List[String] = {
+    List("")
+  }
+
+  def translateSkip() : List[String] = {
+    List("")
+  }
+
+  def translateCommand(cmd : AbstractSyntaxTree.CmdT.Cmd, expr : AbstractSyntaxTree.Expr) : List[String] = {
+    List("")
+  }
+
+  def translateFunction(returnType : AbstractSyntaxTree.DeclarationType, 
+                          ident : AbstractSyntaxTree.IdentLiteral, 
+                          types : List[(AbstractSyntaxTree.DeclarationType, 
+                            AbstractSyntaxTree.IdentLiteral)], 
+                          code : Stat) : List[String] = {
+    List("")
+  }
 }
