30853d41f5b3ee7390870604f4cc660e8e275432
rob <rgw20@ic.ac.uk>
2023-02-21 18:35:05 +0000

    Change binop, unop, if in translator to translate to TAC

--- src/main/scala/wacc/TAC.scala
+++ src/main/scala/wacc/TAC.scala
@@ -4,23 +4,24 @@ import wacc.AbstractSyntaxTree.UnaryOpType.UnOp
 import wacc.AbstractSyntaxTree.BinaryOpType.BinOp
 import wacc.AbstractSyntaxTree.CmdT.Cmd
 
-class TAC {
-
-  case class BinaryOp(op: BinOp, t1: Operand, t2: Operand, res: TRegister) extends TAC
-  case class UnaryOp(op: UnOp, t1: Operand, res: TRegister) extends TAC
-  case class Assignment(t1: Operand, res: TRegister) extends TAC
-  case class IfStat(t1: Operand, goto: Label) extends TAC
-  case class Command(cmd: Cmd, t1: Operand) extends TAC
-  case class PushParam(t1: Operand) extends TAC
-  case class PopParam(t1: TRegister) extends TAC
-  case class Call(f: Label) extends TAC
-  case class BeginFunc() extends TAC
-  case class EndFunc() extends TAC
+object TAC {
+    sealed trait TAC
+  
+  case class BinaryOpTAC(op: BinOp, t1: Operand, t2: Operand, res: TRegister) extends TAC
+  case class UnaryOpTAC(op: UnOp, t1: Operand, res: TRegister) extends TAC
+  case class AssignmentTAC(t1: Operand, res: TRegister) extends TAC
+  case class IfTAC(t1: Operand, goto: Label) extends TAC
+  case class CommandTAC(cmd: Cmd, t1: Operand) extends TAC
+  case class PushParamTAC(t1: Operand) extends TAC
+  case class PopParamTAC(t1: TRegister) extends TAC
+  case class CallTAC(f: Label) extends TAC
+  case class BeginFuncTAC() extends TAC
+  case class EndFuncTAC() extends TAC
+  case class GOTO(label: Label) extends TAC
+  case class Label() extends TAC
 
   sealed trait Operand
   class TRegister() extends Operand
   class Literal() extends Operand
-
-  class Label()
+  
 }
-
--- src/main/scala/wacc/Translator.scala
+++ src/main/scala/wacc/Translator.scala
@@ -1,18 +1,71 @@
 package wacc
 
-import wacc.AbstractSyntaxTree.{ASTNode, Stat, SkipStat, BeginEndStat, Command, Program, Func}
+import wacc.AbstractSyntaxTree.{ASTNode, Stat, SkipStat, BeginEndStat, 
+  Command, Program, Func, Expr, BinaryOp, UnaryOp}
+import wacc.AbstractSyntaxTree.BinaryOpType.BinOp
+import wacc.AbstractSyntaxTree.UnaryOpType.UnOp
+import wacc.TAC._
+import wacc.AbstractSyntaxTree.IfStat
+import wacc.AbstractSyntaxTree.Assignment
 
 object Translator {
-  //TODO: Translate each ASTNode into ARM
+  //TODO: Translate each ASTNode into TAC
   
-  def delegateASTNode(node: ASTNode, context : ScopeContext) : List[TAC] = {
+  def delegateASTNode(node: ASTNode, context : ScopeContext) : (List[TAC], TRegister) = {
+    // Check if ASTNode has already been calculated
+    checkMap() match {
+      case Something => 
+      case Nothing => 
+    }
     node match {
-      case Program(funcs, stat) => translateProgram(funcs, stat, context)
-      case BeginEndStat(stat) => translateBeginEnd(stat)
-      case SkipStat() => translateSkip()
-      case Command(cmd, expr) => translateCommand(cmd, expr)
-      case Func(returnType, ident, types, code) => translateFunction(returnType, ident, types, code)
-      case _ => List()
+      case BinaryOp(op, expr1, expr2) => translateBinOp(op, expr1, expr2)
+      case UnaryOp(op, expr) => translateUnOp(op, expr)
+      case IfStat(cond, stat1, stat2) => translateIfStat(cond, stat1, stat2)
+      case _ => (List(), null)
+    }
+  }
+
+  def translateBinOp(op: BinOp, exp1: Expr, exp2: Expr) = {
+    delegateASTNode(exp1, null) match {
+      case (tacList1, reg1) => {
+        delegateASTNode(exp2, null) match {
+          case (tacList2, reg2) => {
+            val nextReg = nextRegister()
+            (tacList1 ++ tacList2 ++ List(BinaryOpTAC(op, reg1, reg2, nextReg)),
+              nextReg)
+          }
+        }
+      }
+    }
+  }
+
+  def translateUnOp(op: UnOp, exp: Expr): (List[TAC], TRegister) = {
+    delegateASTNode(exp, null) match {
+      case (tacList, reg) => {
+        val nextReg = nextRegister()
+        (tacList ++ List(UnaryOpTAC(op, reg, nextReg)),
+          nextReg)
+      }
+    }
+  }
+
+  def translateIfStat(cond: Expr, stat1: Stat, stat2: Stat): (List[TAC], TRegister) = {
+    delegateASTNode(cond, null) match {
+      case (condList, reg1) => {
+          delegateASTNode(stat1, null) match {
+            case (trueList, reg2) => {
+              delegateASTNode(stat2, null) match {
+                case (falseList, reg3) => {
+                  val l1 = new Label()
+                  val l2 = new Label()
+                  (condList ++ List(IfTAC(reg1, l1)) ++ falseList ++ List(GOTO(l2), l1) ++ trueList ++ List(l2),
+                    null)
+                }
+              }
+            }
+          }
+        
+      }
     }
   }
 
