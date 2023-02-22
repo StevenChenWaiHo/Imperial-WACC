4f251bb3ef4b48a372db57b8dfe59dadc3aebc60
rob <rgw20@ic.ac.uk>
2023-02-22 20:12:28 +0000

    Add toString methods for debugging

--- src/main/scala/wacc/TAC.scala
+++ src/main/scala/wacc/TAC.scala
@@ -6,10 +6,12 @@ import wacc.AbstractSyntaxTree.CmdT.Cmd
 
 object TAC {
     sealed trait TAC
-  
+
   case class BinaryOpTAC(op: BinOp, t1: Operand, t2: Operand, res: TRegister) extends TAC
   case class UnaryOpTAC(op: UnOp, t1: Operand, res: TRegister) extends TAC
-  case class AssignmentTAC(t1: Operand, res: TRegister) extends TAC
+  final case class AssignmentTAC(t1: Operand, res: TRegister) extends TAC {
+    override def toString(): String = res.toString + " = " + t1.toString()
+  }
   case class IfTAC(t1: Operand, goto: Label) extends TAC
   case class CommandTAC(cmd: Cmd, t1: Operand) extends TAC
   case class PushParamTAC(t1: Operand) extends TAC
@@ -21,7 +23,23 @@ object TAC {
   case class Label(name: String = "label") extends TAC
 
   sealed trait Operand
-  class TRegister(num: Int) extends Operand with TAC
-  class Literal(int: Int) extends Operand
+  class TRegister(num: Int) extends Operand with TAC {
+    override def toString(): String = "_T" + num
+  }
+  class LiteralTAC() extends Operand
+    class IdentLiteralTAC(name: String) extends LiteralTAC {
+      override def toString(): String = name
+    }
+    class IntLiteralTAC(value: Int) extends LiteralTAC {
+      override def toString(): String = value.toString()
+    }
+    class StringLiteralTAC(str: String) extends LiteralTAC {
+      override def toString(): String = "\"" + str + "\""
+    }
+    class BoolLiteralTAC(b: Boolean) extends LiteralTAC
+    class CharLiteralTAC(c: Char) extends LiteralTAC
+  class ArrayOp(elems: List[Operand]) extends Operand {
+    override def toString(): String = "[" + elems + "]"
+  }
   
 }
