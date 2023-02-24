11627b864a38979b06c0792f8274b9c5705a70d1
rob <rgw20@ic.ac.uk>
2023-02-24 11:35:56 +0000

    Remove unused imports causing error

--- src/main/scala/wacc/Assembler.scala
+++ src/main/scala/wacc/Assembler.scala
@@ -1,10 +1,7 @@
 package wacc
 
-import sun.jvm.hotspot.asm.Operand
 import wacc.AbstractSyntaxTree.{ASTNode, BeginEndStat, BinaryOpType, Command, Func, Program, SkipStat, Stat}
 import wacc.TAC.{ArrayElemTAC, ArrayOp, BinaryOpTAC, BoolLiteralTAC, CharLiteralTAC, IdentLiteralTAC, IntLiteralTAC, LiteralTAC, StringLiteralTAC, TAC, TRegister}
-
-import javax.print.attribute.standard.Destination
 
 object Assembler {
   val stack = Array[Register]()
