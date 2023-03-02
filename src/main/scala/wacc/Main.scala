8baeaed34e1b30d0c3b36b49a1d8f2f28357a433
rob <rgw20@ic.ac.uk>
2023-03-02 14:37:20 +0000

    Fix file output

--- src/main/scala/wacc/Main.scala
+++ src/main/scala/wacc/Main.scala
@@ -65,7 +65,7 @@ object Main {
       val outputFilename = inputFilename.replace(".wacc", ".s")
       val outputFile = new File(outputFilename)
       val fileWriter = new BufferedWriter(new FileWriter(outputFile))
-      for(line <- result) fileWriter.write(line + "\n")
+      for(line <- result) fileWriter.write(line)
       fileWriter.close()
     }
     println("\n\nCompilation Successful!")
