0be160f72668c2c1d4fe00c04883c36e21eb286b
shc20 <shc20@ic.ac.uk>
2023-03-05 15:49:55 +0000

    fix_nestedPairLeftAssign

--- src/main/scala/wacc/Assembler.scala
+++ src/main/scala/wacc/Assembler.scala
@@ -389,22 +389,28 @@ class Assembler {
   // bl malloc
   // push ptrReg (remove?)
   // mov ptrReg r0
-  // Pop r0
   // str pairElemReg, [ptrReg, #0]
-  // mov fstReg ptrReg
+  // mov r0 pairElemReg
+  // mov pairElemReg ptrReg
   // pop ptrReg (remove?)
+  // Pop r0
   // push pairElemReg
+  // mov pairElemReg r0
   def assemblePairElem(pairElemType: DeclarationType, pairPos: PairElemT.Elem, ptrReg: TRegister, pairElem: TRegister): AssemblerState = {
-    translatePush("", List(r0)) :: 
     translateMove("", r0, new ImmediateInt(getTypeSize(pairElemType))) ::
       translateBranchLink("", new BranchString("malloc")) ::
       translatePush("", List(translateRegister(ptrReg))) :: // TODO: Remove?
       translateMove("", translateRegister(ptrReg), r0) ::
-      translatePop("", List(r0)) :: 
       translateStr(getInstructionType(pairElemType), translateRegister(pairElem), translateRegister(ptrReg), new ImmediateInt(0)) ::
+        translateMove("", r0, translateRegister(pairElem)) ::
       translateMove("", translateRegister(pairElem), translateRegister(ptrReg)) ::
       translatePop("", List(translateRegister(ptrReg))) :: // TODO: Remove?
-      translatePush("", List(translateRegister(pairElem)))
+      translatePush("", List(translateRegister(pairElem))) ::
+      translateMove("", translateRegister(pairElem), r0)
+  }
+
+  def assembleLDR(datatype: DeclarationType, srcReg: TRegister, ptrReg: TRegister): AssemblerState = {
+    translateLdr("", translateRegister(srcReg), translateRegister(ptrReg), new ImmediateInt(-4))
   }
 
   def assembleUnaryOp(op: UnaryOpType.UnOp, t1: Operand, res: TRegister): AssemblerState = {
