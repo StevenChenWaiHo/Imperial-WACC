bb3237d213267fa92bdde2e8d241f9824dde5c88
tyw21 <tyw21@ic.ac.uk>
2023-03-14 02:36:13 +0000

    Moved files

---
+++ src/main/scala/wacc/arm11/ARM11Assembler.scala
@@ -0,0 +1,205 @@
+package wacc
+
+import wacc.ARM11AssemblerTypes._
+import wacc.AssemblerTypes._
+import wacc.FinalIR._
+import collection.mutable
+
+<<<<<<<< HEAD:src/main/scala/wacc/arm11/ARM11LowLevelAssembler.scala
+object ARM11LowLevelAssembler {
+========
+//TODO Change all to x86_64 Architecture
+
+object X86Assembler {
+>>>>>>>> 53ca949 (Renamed x86asm):src/main/scala/wacc/x86/X86Assembler.scala
+
+  def assemble(irCode: List[FinalIR], endFuncs: mutable.Map[String, List[FinalIR]]): String = {
+    endFuncsIR = endFuncs
+    (irCode.map(assembleIR) ++ endFuncsToList()).mkString("\n")
+  }
+
+  def assembleIR(irCode: FinalIR): String = {
+    irCode match {
+        case Str(cond, src, operand, dst) => assembleStr(cond, src, operand, dst)
+        case StrPre(cond, src, operand, dst) => assembleStrPre(cond, src, operand, dst)
+        case Ldr(cond, src, operand, dst) => assembleLdr(cond, src, operand, dst)
+        case Push(cond, regs) => assemblePush(cond, regs)
+        case Pop(cond, regs) => assemblePop(cond, regs)
+        case Add(cond, flag, op1, op2, dst) => assembleAdd(cond, flag, op1, op2, dst)
+        case Sub(cond, flag, op1, op2, dst) => assembleSub(cond, flag, op1, op2, dst)
+        case Rsb(cond, flag, op1, op2, dst) => assembleRsb(cond, flag, op1, op2, dst)
+        case Mul(cond, flag, op1, op2, dst) => assembleMul(cond, flag, op1, op2, dst)
+        case Smull(cond, flag, dst, op11, op1, op2) => assembleSmull(cond, flag, dst, op11, op1, op2)
+        case Mov(cond, src, dst) => assembleMove(cond, src, dst)
+        case Branch(cond, name) => assembleBranch(cond, name)
+        case BranchLink(cond, name) => assembleBranchLink(cond, name)
+        case Cmp(cond, op1, op2) => assembleCmp(cond, op1, op2)
+        case Global(name) => assembleGlobal(name)
+        case Lbl(name) => assembleLabel(name)
+        case Comment(str) => assembleComment(str)
+        case DataSeg() => assembleDataSeg()
+        case TextSeg() => assembleTextSeg()
+        case AsciiZ(str) => assembleAsciiZ(str)
+        case Word(len) => assembleWord(len)
+        case Special(str) => str
+    }
+  }
+
+  var endFuncsIR = collection.mutable.Map[String, List[FinalIR]]()
+
+  def addEndFunc(name: String, code: List[FinalIR]): Unit = {
+    if (!endFuncsIR.contains(name)) {
+      endFuncsIR.addOne(name, code)
+    }
+  }
+
+  def endFuncsToList(): List[String] = {
+    endFuncsIR.toList.map(entry => entry match {
+      case (name, code) => code.map(c => assembleIR(c))
+    }).flatten
+  }
+
+  def assembleStr(condition: String, src: LHSop, operand: LHSop, dst: Register): String = {
+    "str" + ldrStrAssist(condition, src, operand, dst)
+  }
+
+  def assembleStrPre(condition: String, src: LHSop, operand: LHSop, dst: Register): String = {
+    "str" + ldrStrAssist(condition, src, operand, dst).toString + "!".toString()
+  }
+
+  
+  def assembleLdr(condition: String, src: Register, operand: LHSop, dst: Register): String = {
+    "ldr" + ldrStrAssist(condition, src, operand, dst)
+  }
+
+  def ldrStrAssist(condition: String, src: LHSop, operand: LHSop, dst: Register): String = {
+    var str = condition + " " + dst.toString + ", "
+    operand match {
+      case ARM11ImmediateInt(x) => {
+        str = str + "[" + src.toString + ", #" + x + "]"
+      }
+      case ARM11LabelString(x) => {
+        str = str + "=" + x
+      }
+    }
+    str
+  }
+
+  def assemblePush(condition: String, registers: List[Register]): String = {
+    "push" + pushPopAssist(condition, registers)
+  }
+
+  def assemblePop(condition: String, registers: List[Register]): String = {
+    "pop" + pushPopAssist(condition, registers)
+  }
+
+  def pushPopAssist(condition: String, registers: List[Register]): String = {
+    var str = condition + " {"
+    for (register <- registers) {
+      if (register != registers.last) {
+        str = str + register.toString + ", "
+      } else {
+        str = str + register.toString
+      }
+    }
+    str + "}"
+  }
+
+  def assembleAdd(condition: String, setflag: Suffi, op1: LHSop, op2: LHSop, dst: LHSop): String = {
+    "add" + addSubMulAssist(condition, setflag, op1, op2, dst)
+  }
+
+  def assembleSub(condition: String, setflag: Suffi, op1: LHSop, op2: LHSop, dst: LHSop): String = {
+    "sub" + addSubMulAssist(condition, setflag, op1, op2, dst)
+  }
+
+  def assembleRsb(condition: String, setflag: Suffi, op1: LHSop, op2: LHSop, dst: LHSop): String = {
+    "rsb" + addSubMulAssist(condition, setflag, op1, op2, dst)
+  }
+
+  def assembleMul(condition: String, setflag: Suffi, op1: LHSop, op2: LHSop, dst: LHSop): String = {
+    "mul" + addSubMulAssist(condition, setflag, op1, op2, dst)
+  }
+
+  def assembleSmull(condition: String, setflag: Suffi, dst: LHSop, op11: LHSop, op1: LHSop, op2: LHSop): String = {
+    "smull" + fourMulAssist(condition, setflag, dst, op11, op1, op2)
+  }
+
+  def fourMulAssist(condition: String, setflag: Suffi, destinationLow: LHSop, destinationHigh: LHSop,
+                    sourceRegister: LHSop, operand: LHSop): String = {
+    addEndFunc("_errOverflow", new ARM11HelperFunctions().assemble_errOverflow())
+    addEndFunc("_prints", new ARM11HelperFunctions().assemble_prints())
+
+    condition + setflag + " " + destinationLow + "," + " " + destinationHigh + "," + " " + op1 +
+      "," + " " + op2 +
+      "\ncmp " + destinationHigh + ", " + destinationLow + ", asr #31" +
+      "\nbne _errOverflow"
+  }
+
+
+  def addSubMulAssist(condition: String, setflag: Suffi, op1: LHSop, op2: LHSop, dst: LHSop): String = {
+    addEndFunc("_errOverflow", new ARM11HelperFunctions().assemble_errOverflow())
+    addEndFunc("_prints", new ARM11HelperFunctions().assemble_prints())
+    condition + setflag + " " + dst + ", " + op1 + ", " + op2 + "\nblvs _errOverflow"
+  }
+
+  def assembleCmp(condition: String, op1: LHSop, op2: LHSop): String = {
+    "cmp" + condition + " " + op1.toString + ", " + op2.toString
+  }
+
+  // Determine when a mov is allowed
+  def checkMovCases(i: Int): Boolean = {
+    for (j <- 0 to 12) {
+      val mask = (0xFF << (j * 2))
+      if ((i & ~mask) == 0) return true
+    }
+    if ((i & ~0xFC000003) == 0) return true
+    if ((i & ~0xF000000F) == 0) return true
+    if ((i & ~0xC000003F) == 0) return true
+    false
+  }
+
+  def assembleMove(condition: String, src: LHSop, dst: Register): String = {
+    src match {
+      case ARM11ImmediateInt(i) if !checkMovCases(i) => "ldr " + condition + " " + dst.toString() + ", =" + i
+      case _ => "mov" + condition + " " + dst.toString + ", " + src.toString()
+    }
+  }
+
+  def assembleBranch(condition: String, name: String): String = {
+    "b" + condition + " " + name
+  }
+
+  def assembleBranchLink(condition: String, name: LHSop): String = {
+    "bl" + condition + " " + name
+  }
+
+  def assembleGlobal(name: String) = {
+    ".global " + name
+  }
+
+  def assembleLabel(name: String): String = {
+      name + ":"
+  }
+
+  def assembleComment(comment: String): String = {
+    "@ " + comment
+  }
+
+  def assembleDataSeg(): String = {
+    ".data"
+  }
+
+  def assembleTextSeg(): String = {
+    ".text"
+  }
+
+  def assembleAsciiZ(str: String): String = {
+    ".asciz " + str
+  }
+
+  def assembleWord(len: Int): String = {
+    ".word " + len.toString()
+  }
+
+}
---
+++ src/main/scala/wacc/arm11/HelperFunctions.scala
@@ -0,0 +1,311 @@
+package wacc
+
+import wacc.ARM11HighLevelAssembler._
+import wacc.ARM11AssemblerTypes._
+import wacc.ArchitectureType._
+import wacc.AssemblerTypes._
+import wacc.RegisterAllocator._
+import wacc.TAC._
+import wacc.FinalIR.FinalIR
+
+import scala.collection.mutable.ListBuffer
+
+class ARM11HelperFunctions {
+  private[this] val state = new AssemblerState(ListBuffer(r4, r5, r6, r7, r8, r9, r10, r11), ArchitectureType.ARM11)
+
+  implicit private[this] def toStrings(state: AssemblerState) = state.code.toList
+
+  implicit private[this] def updateState(instr: FinalIR): AssemblerState = state.addInstruction(instr)
+
+  def assemble_errNull(): List[FinalIR] = {
+    val sLbl = new Label(".L._errNull_str0")
+    assembleTAC(DataSegmentTAC()) ++
+    assembleTAC(Comments("length of " + sLbl.name)) ++
+    assembleTAC(StringLengthDefinitionTAC(45, sLbl)) ++
+    assembleTAC(StringDefinitionTAC("fatal error: null pair dereferenced or freed\n", sLbl)) ++
+    assembleTAC(TextSegmentTAC()) ++
+    assembleTAC(Label("_errNull")) ++
+    (FinalIR.Ldr("", r0, ARM11LabelString(".L._errNull_str0"), r0) ::
+    FinalIR.BranchLink("", ARM11BranchString("_prints")) ::
+    FinalIR.Mov("", ARM11ImmediateInt(255), r0) ::
+    FinalIR.BranchLink("", ARM11BranchString("exit")))
+  }
+
+  def assemble_freepair(): List[FinalIR] = {
+    assembleTAC(TextSegmentTAC()) ++
+    assembleTAC(Label("_freepair")) ++
+    (FinalIR.Push("", List(lr)) ::
+    FinalIR.Mov("", r0, r1) ::
+    FinalIR.Cmp("", r1, ARM11ImmediateInt(0)) ::
+    FinalIR.BranchLink("eq", ARM11BranchString("_errNull")) ::
+    FinalIR.Ldr("", r1, ARM11ImmediateInt(0), r0) ::
+    FinalIR.Push("", List(r1)) ::
+    FinalIR.BranchLink("", ARM11BranchString("free")) ::
+    FinalIR.Pop("", List(r1)) ::
+    FinalIR.Ldr("", r1, ARM11ImmediateInt(POINTER_BYTE_SIZE), r0) ::
+    FinalIR.Push("", List(r1)) ::
+    FinalIR.BranchLink("", ARM11BranchString("free")) ::
+    FinalIR.Pop("", List(r1)) ::
+    FinalIR.Mov("", r1, r0) ::
+    FinalIR.Push("", List(r1)) ::
+    FinalIR.BranchLink("", ARM11BranchString("free")) ::
+    FinalIR.Pop("", List(r1)) ::
+    FinalIR.Pop("", List(pc)) :: List())
+  }
+
+  
+  def assemble_errDivZero(): List[FinalIR] = {
+    val sLbl = Label(".L._errDivZero_str0")
+    assembleTAC(DataSegmentTAC()) ++
+    assembleTAC(Comments("length of " + sLbl.name)) ++
+    assembleTAC(StringLengthDefinitionTAC(40, sLbl)) ++
+    assembleTAC(StringDefinitionTAC("fatal error: division or modulo by zero\n", sLbl)) ++
+    assembleTAC(TextSegmentTAC()) ++
+    assembleTAC(Label("_errDivZero")) ++
+    (FinalIR.Ldr("", null, ARM11LabelString(sLbl.name), r0) ::
+    FinalIR.BranchLink("", ARM11BranchString("_prints")) ::
+    FinalIR.Mov("", ARM11ImmediateInt(255), r0) ::
+    FinalIR.BranchLink("", ARM11BranchString("exit")))
+  }
+
+  def assemble_errOverflow(): List[FinalIR] = {
+    val sLbl = Label(".L._errOverflow_str0")
+    assembleTAC(DataSegmentTAC()) ++
+    assembleTAC(Comments("length of " + sLbl.name)) ++
+    assembleTAC(StringLengthDefinitionTAC(52, sLbl)) ++
+    assembleTAC(StringDefinitionTAC("fatal error: integer overflow or underflow\n", sLbl)) ++
+    assembleTAC(TextSegmentTAC()) ++
+    assembleTAC(Label("_errOverflow")) ++
+    (FinalIR.Ldr("", null, ARM11LabelString(sLbl.name), r0) ::
+    FinalIR.BranchLink("", ARM11BranchString("_prints")) ::
+    FinalIR.Mov("", ARM11ImmediateInt(255), r0) ::
+    FinalIR.BranchLink("", ARM11BranchString("exit")))
+  }
+
+  // Special calling convention: array ptr passed in R3, index in R10, LR (R14) is used as general register, and return into R3
+  // ie r3 = r3[r10] (from reference compiler)
+  // we instead use r0 = r3[r2]
+  def assemble_arrLoad(): List[FinalIR] = {
+    assembleTAC(Label("_arrLoad")) ++
+    (FinalIR.Push("", List(lr)) ::
+      FinalIR.Cmp("", r2, ARM11ImmediateInt(0)) ::
+      FinalIR.Mov("lt", r2, r1) ::
+      FinalIR.BranchLink("lt", ARM11BranchString("_boundsCheck")) ::
+      FinalIR.Ldr("", r3, ARM11ImmediateInt(-POINTER_BYTE_SIZE), lr) ::
+      FinalIR.Cmp("", r2, lr) ::
+      FinalIR.Mov("ge", r2, r1) ::
+      FinalIR.BranchLink("ge", ARM11BranchString("_boundsCheck")) ::
+      FinalIR.Ldr("", r3, LogicalShiftLeft(r2, Right(2)), r0) ::
+      FinalIR.Pop("", List(pc)) :: List())
+  }
+
+  // Special calling convention: array ptr passed in R3, index in R10, value to store in R8, LR (R14) is used as general register
+  // ie r3[r10] = r8 (from reference compiler)
+  // we instead use r3[r0] = r2
+  def assemble_arrStore(): List[FinalIR] = {
+    assembleTAC(Label("_arrStore")) ++
+    (FinalIR.Push("", List(lr)) ::
+      FinalIR.Cmp("", r0, ARM11ImmediateInt(0)) ::
+      FinalIR.Mov("lt", r0, r1) :: // r0 < 0
+      FinalIR.BranchLink("lt", ARM11BranchString("_boundsCheck")) ::
+      FinalIR.Ldr("", r3, ARM11ImmediateInt(-POINTER_BYTE_SIZE), lr) ::
+      FinalIR.Cmp("", r0, lr) ::
+      FinalIR.Mov("ge", r0, r1) :: // r0 >= lr
+      FinalIR.BranchLink("ge", ARM11BranchString("_boundsCheck")) ::
+      FinalIR.Str("", LogicalShiftLeft(r0, Right(2)), r2, r3) :: // TODO: Logical shift does not work
+      FinalIR.Pop("", List(pc)))
+  }
+
+  def assemble_boundsCheck(): List[FinalIR] = {
+    val sLbl = new Label(".L._boundsCheck_str_0")
+    assembleTAC(DataSegmentTAC()) ++
+    assembleTAC(Comments("length of " + sLbl.name)) ++
+    assembleTAC(StringLengthDefinitionTAC(42, sLbl)) ++
+    assembleTAC(StringDefinitionTAC("fatal error: array index %d out of bounds\n", sLbl)) ++
+    assembleTAC(TextSegmentTAC()) ++
+    assembleTAC(Label("_boundsCheck")) ++
+    (FinalIR.Ldr("", r0, ARM11LabelString(sLbl.name), r0) ::
+      FinalIR.BranchLink("", ARM11BranchString("printf")) ::
+      FinalIR.Mov("", ARM11ImmediateInt(0), r0) ::
+      FinalIR.BranchLink("", ARM11BranchString("fflush")) ::
+      FinalIR.Mov("", ARM11ImmediateInt(255), r0) ::
+      FinalIR.BranchLink("", ARM11BranchString("exit")))
+  }
+
+  def assemble_print(pType: String): List[FinalIR] = {
+    pType match {
+      case "_prints" => assemble_prints()
+      case "_printi" => assemble_printi()
+      case "_printc" => assemble_printc()
+      case "_printb" => assemble_printb()
+      case "_printp" => assemble_printp()
+      case "_println" => assemble_println()
+      case _ => assemble_prints()
+    }
+  }
+
+   def assemble_printp(): List[FinalIR] = {
+    val sLbl = new Label(".L._printp_str0")
+    assembleTAC(DataSegmentTAC()) ++
+    assembleTAC(Comments("length of " + sLbl.name)) ++
+    assembleTAC(StringLengthDefinitionTAC(2, sLbl)) ++
+    assembleTAC(StringDefinitionTAC("%p", sLbl)) ++
+    assembleTAC(TextSegmentTAC()) ++
+    assembleTAC(Label("_printp")) ++
+    (FinalIR.Push("", List(lr)) ::
+        FinalIR.Mov("", r0, r1) ::
+        FinalIR.Ldr("", r0, ARM11LabelString(sLbl.name), r0) ::
+        FinalIR.BranchLink("", ARM11BranchString("printf")) ::
+        FinalIR.Mov("", ARM11ImmediateInt(0), r0) ::
+        FinalIR.BranchLink("", ARM11BranchString("fflush")) ::
+        FinalIR.Pop("", List(pc)))
+   }
+
+  def assemble_prints(): List[FinalIR] = {
+    val sLbl = new Label(".L._prints_str0")
+    assembleTAC(DataSegmentTAC()) ++
+      assembleTAC(Comments("length of " + sLbl.name)) ++
+      assembleTAC(StringLengthDefinitionTAC(4, sLbl)) ++
+      assembleTAC(StringDefinitionTAC("%.*s", sLbl)) ++
+      assembleTAC(TextSegmentTAC()) ++
+      assembleTAC(Label("_prints")) ++
+      (FinalIR.Push("", List(lr)) ::
+        FinalIR.Mov("", r0, r2) ::
+        FinalIR.Ldr("", r0, ARM11ImmediateInt(-POINTER_BYTE_SIZE), r1) ::
+        FinalIR.Ldr("", r0, ARM11LabelString(sLbl.name), r0) ::
+        FinalIR.BranchLink("", ARM11BranchString("printf")) ::
+        FinalIR.Mov("", ARM11ImmediateInt(0), r0) ::
+        FinalIR.BranchLink("", ARM11BranchString("fflush")) ::
+        FinalIR.Pop("", List(pc)))
+  }
+
+  def assemble_printc(): List[FinalIR] = {
+    val sLbl = new Label(".L._printc_str0")
+    assembleTAC(DataSegmentTAC()) ++
+      assembleTAC(Comments("length of " + sLbl.name)) ++
+      assembleTAC(StringLengthDefinitionTAC(2, sLbl)) ++
+      assembleTAC(StringDefinitionTAC("%c", sLbl)) ++
+      assembleTAC(TextSegmentTAC()) ++
+      assembleTAC(Label("_printc")) ++
+      (FinalIR.Push("", List(lr)) ::
+        FinalIR.Mov("", r0, r1) ::
+        FinalIR.Ldr("", r0, ARM11LabelString(sLbl.name), r0) ::
+        FinalIR.BranchLink("", ARM11BranchString("printf")) ::
+        FinalIR.Mov("", ARM11ImmediateInt(0), r0) ::
+        FinalIR.BranchLink("", ARM11BranchString("fflush")) ::
+        FinalIR.Pop("", List(pc)))
+  }
+
+  def assemble_printi(): List[FinalIR] = {
+    val sLbl = new Label(".L._printi_str0")
+    assembleTAC(DataSegmentTAC()) ++
+      assembleTAC(Comments("length of " + sLbl.name)) ++
+      assembleTAC(StringLengthDefinitionTAC(2, sLbl)) ++
+      assembleTAC(StringDefinitionTAC("%d", sLbl)) ++
+      assembleTAC(TextSegmentTAC()) ++
+      assembleTAC(Label("_printi")) ++
+      (FinalIR.Push("", List(lr)) ::
+        FinalIR.Mov("", r0, r1) ::
+        FinalIR.Ldr("", r0, ARM11LabelString(sLbl.name), r0) ::
+        FinalIR.BranchLink("", ARM11BranchString("printf")) ::
+        FinalIR.Mov("", ARM11ImmediateInt(0), r0) ::
+        FinalIR.BranchLink("", ARM11BranchString("fflush")) ::
+        FinalIR.Pop("", List(pc)))
+  }
+
+  def assemble_println(): List[FinalIR] = {
+    val sLbl = new Label(".L._println_str0")
+    assembleTAC(DataSegmentTAC()) ++
+      assembleTAC(Comments("length of " + sLbl.name)) ++
+      assembleTAC(StringLengthDefinitionTAC(0, sLbl)) ++
+      assembleTAC(StringDefinitionTAC("", sLbl)) ++
+      assembleTAC(TextSegmentTAC()) ++
+      assembleTAC(Label("_println")) ++
+      (FinalIR.Push("", List(lr)) ::
+        FinalIR.Ldr("", r0, ARM11LabelString(sLbl.name), r0) ::
+        FinalIR.BranchLink("", ARM11BranchString("puts")) ::
+        FinalIR.Mov("", ARM11ImmediateInt(0), r0) ::
+        FinalIR.BranchLink("", ARM11BranchString("fflush")) ::
+        FinalIR.Pop("", List(pc)))
+  }
+
+  def assemble_printb(): List[FinalIR] = {
+    val fLbl = new Label(".L._printb_str0")
+    val tLbl = new Label(".L._printb_str1")
+    val sLbl = new Label(".L._printb_str2")
+
+    assembleTAC(DataSegmentTAC()) ++
+      assembleTAC(Comments("length of " + fLbl.name)) ++
+      assembleTAC(StringLengthDefinitionTAC(5, fLbl)) ++
+      assembleTAC(StringDefinitionTAC("false", fLbl)) ++
+      assembleTAC(Comments("length of " + tLbl.name)) ++
+      assembleTAC(StringLengthDefinitionTAC(4, tLbl)) ++
+      assembleTAC(StringDefinitionTAC("true", tLbl)) ++
+      assembleTAC(Comments("length of " + sLbl.name)) ++
+      assembleTAC(StringLengthDefinitionTAC(4, sLbl)) ++
+      assembleTAC(StringDefinitionTAC("%.*s", sLbl)) ++
+      assembleTAC(TextSegmentTAC()) ++
+      assembleTAC(Label("_printb")) ++
+      (FinalIR.Push("", List(lr)) ::
+        FinalIR.Cmp("", r0, ARM11ImmediateInt(0)) ::
+        FinalIR.Branch("ne", ".L_printb0") ::
+        FinalIR.Ldr("", r0, ARM11LabelString(fLbl.name), r2) ::
+        FinalIR.Branch("", ".L_printb1") ::
+        assembleTAC(Label(".L_printb0"))) ++
+      (FinalIR.Ldr("", r0, ARM11LabelString(tLbl.name), r2) ::
+        assembleTAC(Label(".L_printb1"))) ++
+      (FinalIR.Ldr("", r2, ARM11ImmediateInt(-POINTER_BYTE_SIZE), r1) ::
+        FinalIR.Ldr("", r0, ARM11LabelString(sLbl.name), r0) ::
+        FinalIR.BranchLink("", ARM11BranchString("printf")) ::
+        FinalIR.Mov("", ARM11ImmediateInt(0), r0) ::
+        FinalIR.BranchLink("", ARM11BranchString("fflush")) ::
+        FinalIR.Pop("", List(pc)))
+  }
+
+  def assemble_read(rType: String): List[FinalIR] = {
+    rType match {
+      case "_readi" => assemble_readi()
+      case "_readc" => assemble_readc()
+      case _ => assemble_readi()
+    }
+  }
+
+  def assemble_readi(): List[FinalIR] = {
+    val lbl = new Label(".L._readi_str0")
+    List(
+      DataSegmentTAC(),
+      Comments("length of " + lbl.name),
+      StringLengthDefinitionTAC(2, lbl),
+      StringDefinitionTAC("%d", lbl),
+      TextSegmentTAC(),
+      Label("_readi")).map(tac => assembleTAC(tac)).flatten ++
+      (FinalIR.Push("", List(lr)) ::
+      FinalIR.StrPre("", sp, ARM11ImmediateInt(-POINTER_BYTE_SIZE), r0) ::
+      FinalIR.Mov("", sp, r1) ::
+      FinalIR.Ldr("", null, ARM11LabelString(lbl.name), r0) ::
+      FinalIR.BranchLink("", ARM11BranchString("scanf")) ::
+      FinalIR.Ldr("", sp, ARM11ImmediateInt(0), r0) ::
+      FinalIR.Add("", ARM11None(), sp, ARM11ImmediateInt(POINTER_BYTE_SIZE), sp) ::
+      FinalIR.Pop("", List(pc)))
+  }
+
+  def assemble_readc(): List[FinalIR] = {
+    val lbl = new Label(".L._readc_str0")
+    List(
+      DataSegmentTAC(),
+      Comments("length of " + lbl.name),
+      StringLengthDefinitionTAC(3, lbl),
+      StringDefinitionTAC(" %c", lbl),
+      TextSegmentTAC(),
+      Label("_readc")).map(tac => assembleTAC(tac)).flatten ++
+      (FinalIR.Push("", List(lr)) :: 
+      FinalIR.StrPre("b", sp, ARM11ImmediateInt(-1), r0) ::
+      FinalIR.Mov("", sp, r1) :: 
+      FinalIR.Ldr("", null, ARM11LabelString(lbl.name), r0) ::
+      FinalIR.BranchLink("", ARM11BranchString("scanf")) ::
+      FinalIR.Ldr("sb", sp, ARM11ImmediateInt(0), r0) ::
+      FinalIR.Add("", ARM11None(), sp, ARM11ImmediateInt(1), sp) ::
+      FinalIR.Pop("", List(pc)))
+  }
+
+}
