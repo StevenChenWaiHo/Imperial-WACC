43dd3d6cb9bd6fc2cca91c539cf1ebc4d5897c6b
tyw21 <tyw21@ic.ac.uk>
2023-03-14 17:26:07 +0000

    Fixed output

--- src/main/scala/wacc/x86/X86LowLevelAssembler.scala
+++ src/main/scala/wacc/x86/X86LowLevelAssembler.scala
@@ -55,45 +55,43 @@ object X86LowLevelAssembler {
     }).flatten
   }
 
-  //TODO determine if register size change needed (ie q to l or similar)
-
-  //Uses movq and r registers for now
+  // Uses mov/lea instead of str/ldr
   def assembleStr(condition: String, src: LHSop, operand: LHSop, dst: Register): String = {
-    "movq" + ldrStrAssist(condition, src, operand, dst)
+    "mov" + ldrStrAssist(condition, src, operand, dst)
   }
 
   def assembleStrPre(condition: String, src: LHSop, operand: LHSop, dst: Register): String = {
-    "movq" + ldrStrAssist(condition, src, operand, dst).toString + "!".toString()
+    "mov" + ldrStrAssist(condition, src, operand, dst).toString + "!".toString()
   }
 
   
   def assembleLdr(condition: String, src: Register, operand: LHSop, dst: Register): String = {
-    "movq" + ldrStrAssist(condition, src, operand, dst)
+    "mov" + ldrStrAssist(condition, src, operand, dst)
   }
 
+  //TODO split out lea
   def ldrStrAssist(condition: String, src: LHSop, operand: LHSop, dst: Register): String = {
     println(condition)
     println(src)
     println(operand)
     println(dst)
-    var str = condition + " " + src.toString + ", "
+    var str = condition + " " + dst.toString + ", " //src null issue
     operand match {
       case ImmediateInt(x) => {
-        str = str + x + "(" + src.toString + ")"
+        str + x + "[" + src.toString + ", #" + x + "]"
       }
       case LabelString(x) => {
-        str = "leaq" + x + "(%rip), " + dst.toString //%rip is instruction pointer
+        " lea" + x + "(%rip), " + dst.toString //%rip is instruction pointer
       }
     }
-    str
   }
 
   def assemblePush(condition: String, registers: List[Register]): String = {
-    "pushq" + pushPopAssist(condition, registers)
+    "push" + pushPopAssist(condition, registers)
   }
 
   def assemblePop(condition: String, registers: List[Register]): String = {
-    "popq" + pushPopAssist(condition, registers)
+    "pop" + pushPopAssist(condition, registers)
   }
 
   def pushPopAssist(condition: String, registers: List[Register]): String = {
@@ -109,23 +107,23 @@ object X86LowLevelAssembler {
   }
 
   def assembleAdd(condition: String, setflag: Suffi, op1: LHSop, op2: LHSop, dst: LHSop): String = {
-    "addq" + addSubMulAssist(condition, setflag, op1, op2, dst)
+    "add" + addSubMulAssist(condition, setflag, op1, op2, dst)
   }
 
   def assembleSub(condition: String, setflag: Suffi, op1: LHSop, op2: LHSop, dst: LHSop): String = {
-    "subq" + addSubMulAssist(condition, setflag, op1, op2, dst)
+    "sub" + addSubMulAssist(condition, setflag, op1, op2, dst)
   }
 
   def assembleRsb(condition: String, setflag: Suffi, op1: LHSop, op2: LHSop, dst: LHSop): String = {
-    "rsbq" + addSubMulAssist(condition, setflag, op1, op2, dst)
+    "rsb" + addSubMulAssist(condition, setflag, op1, op2, dst)
   }
 
   def assembleMul(condition: String, setflag: Suffi, op1: LHSop, op2: LHSop, dst: LHSop): String = {
-    "mulq" + addSubMulAssist(condition, setflag, op1, op2, dst)
+    "mul" + addSubMulAssist(condition, setflag, op1, op2, dst)
   }
 
   def assembleSmull(condition: String, setflag: Suffi, src: LHSop, op1: LHSop, op2: LHSop, dst: LHSop): String = {
-    "smullq" + fourMulAssist(condition, setflag, dst, src, op1, op2)
+    "smull" + fourMulAssist(condition, setflag, dst, src, op1, op2)
   }
 
   def fourMulAssist(condition: String, setflag: Suffi, destinationLow: LHSop, destinationHigh: LHSop,
@@ -147,7 +145,7 @@ object X86LowLevelAssembler {
   }
 
   def assembleCmp(condition: String, op1: LHSop, op2: LHSop): String = {
-    "cmpq" + condition + " " + op1.toString + ", " + op2.toString
+    "cmp" + condition + " " + op1.toString + ", " + op2.toString
   }
 
   // Determine when a mov is allowed
@@ -163,9 +161,10 @@ object X86LowLevelAssembler {
   }
 
   def assembleMove(condition: String, src: LHSop, dst: Register): String = {
-    src match { //x86 should only use mov
+    condition match { //x86 should only use mov or cmov
       // case ImmediateInt(i) if !checkMovCases(i) => "ldr " + condition + " " + dst.toString() + ", =" + i
-      case _ => "movq" + condition + " " + dst.toString + ", " + src.toString()
+      case _ if condition.isEmpty => "mov" + " " + dst.toString + ", " + src.toString()
+      case _ => "cmov" + condition + " " + dst.toString + ", " + src.toString()
     }
   }
 
@@ -178,7 +177,7 @@ object X86LowLevelAssembler {
   }
 
   def assembleGlobal(name: String) = {
-    ".globl " + name
+    ".intel_syntax noprefix\n.globl " + name
   }
 
   def assembleLabel(name: String): String = {
