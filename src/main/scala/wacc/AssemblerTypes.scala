bf8c4c579772aed2bf87b1b6aafc4d59e1359caf
tyw21 <tyw21@ic.ac.uk>
2023-03-15 16:46:24 +0000

    Abstracted AssemblerTypes

--- src/main/scala/wacc/AssemblerTypes.scala
+++ src/main/scala/wacc/AssemblerTypes.scala
@@ -1,7 +1,7 @@
 package wacc
 
 object AssemblerTypes {
-  class LHSop
+  trait LHSop
 
   trait StackOffset extends LHSop {
     def offset: Int
@@ -19,12 +19,27 @@ object AssemblerTypes {
     def name: String
   }
 
-  class Register extends LHSop with Ordered[Register] {
+  trait Register extends LHSop with Ordered[Register] {
     import scala.math.Ordered.orderingToOrdered
     def compare(that: Register): Int
   }
 
+  // ARM/X86
+  trait GeneralRegister extends Register // r0-14?/other regs
+  trait SPRegister extends Register // sp/rsp
+  trait FPRegister extends Register // fp/rbp
+  // are the below needed
+  trait LinkRegister extends Register // lr/??
+  trait PCRegister extends Register // pc/??
+  //rbx as basepointer?
+
   //registers should be defined here? (in ARM format, more registers)
 
-  class Suffi
+  trait Suffi
+
+  trait Control extends Suffi
+  trait Extension extends Suffi
+  trait Status extends Suffi
+  trait Flags extends Suffi
+  trait None extends Suffi
 }
