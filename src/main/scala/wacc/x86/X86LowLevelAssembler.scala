cd29cfde3d819b6f217226bdc56155cfe7ee303d
tyw21 <tyw21@ic.ac.uk>
2023-03-16 19:25:30 +0000

    Adapted some to X86

--- src/main/scala/wacc/x86/X86LowLevelAssembler.scala
+++ src/main/scala/wacc/x86/X86LowLevelAssembler.scala
@@ -116,6 +116,7 @@ object X86LowLevelAssembler {
         str = str + instr + condition + " " + register.toString
       }
     }
+    str
   }
 
   def assembleAdd(condition: String, setflag: Suffi, op1: LHSop, op2: LHSop, dst: LHSop): String = {
