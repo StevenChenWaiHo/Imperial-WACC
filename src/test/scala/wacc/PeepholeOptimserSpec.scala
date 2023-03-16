08477bdd2f540153a47a85459c637adcc66c738c
rob <rgw20@ic.ac.uk>
2023-03-16 23:19:35 +0000

    Add removeNullOp test

--- src/test/scala/wacc/PeepholeOptimserSpec.scala
+++ src/test/scala/wacc/PeepholeOptimserSpec.scala
@@ -13,7 +13,41 @@ class PeepholeOptimserSpec extends AnyFlatSpec {
     assert(!isNullOp(Mov("", r4, r2)))
   }
 
+  def testRemoveNullOp() = {
+    val testCode = List(
+      DataSeg(),
+      TextSeg(),
+      Global("main"),
+      Lbl("main"),
+      Push("", List(fp, lr)),
+      Mov("", sp, fp),
+      Mov("", ImmediateInt(3), r1),
+      Add("", None(), r1, ImmediateInt(0), r1),
+      Mov("", r1, r0),
+      Pop("", List(fp, pc)),
+    )
+
+    val testOutput = List(
+      DataSeg(),
+      TextSeg(),
+      Global("main"),
+      Lbl("main"),
+      Push("", List(fp, lr)),
+      Mov("", sp, fp),
+      Mov("", ImmediateInt(3), r1),
+      Add("", None(), r1, ImmediateInt(0), r1),
+      Mov("", r1, r0),
+      Pop("", List(fp, pc)),
+    )
+
+    assert(PeepholeOptimise(testCode) == testOutput)
+  }
+
   "Peephole Optimiser" can "identify null operations" in {
     testNullOpIdentify()
+  }
+
+  "Peephole Optimiser" should "remove null operations" in {
+    testRemoveNullOp()
   }
 }
